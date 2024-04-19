51d0f906c4ec8293d46341777b3d77e96ea70371
samarsamara <166445618+samarsamara@users.noreply.github.com>
2024-04-19 21:00:58 +0300

    Update TransformerLSTM.py

--- environments/TransformerLSTM.py
+++ environments/TransformerLSTM.py
@@ -72,7 +72,7 @@ class TransformerLSTM(nn.Module):
 
 class TransformerLSTM_env(environment.Environment):
     def init_model_arc(self, config):
-        self.model = TransformerLSTM_env(config=config).double()
+        self.model = TransformerLSTM(config=config).double()
 
     def predict_proba(self, data, update_vectors: bool, vectors_in_input=False):
         if vectors_in_input:
