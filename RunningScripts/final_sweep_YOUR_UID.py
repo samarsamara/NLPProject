49983cd8a65f1297d488719082b49cff6809f360
rawanbad <167420452+rawanbad@users.noreply.github.com>
2024-04-21 15:08:43 +0300

    Update final_sweep_YOUR_UID.py

--- RunningScripts/final_sweep_YOUR_UID.py
+++ RunningScripts/final_sweep_YOUR_UID.py
@@ -1,6 +1,6 @@
 import wandb
-YOUR_WANDB_USERNAME = "eilamshapira"
-project = "NLP2024_PROJECT_YOUR_UID"
+YOUR_WANDB_USERNAME = "samar2499"
+project = "Strategy_Transfer_TACL_samar2499"
 
 command = [
         "${ENVIRONMENT_VARIABLE}",
@@ -11,18 +11,19 @@ command = [
     ]
 
 sweep_config = {
-    "name": "LSTM: SimFactor=0/4 for any features representation",
+    "name": "LSTMTransformer: SimFactor=0/4 for any features representation",
     "method": "grid",
     "metric": {
         "goal": "maximize",
         "name": "AUC.test.max"
     },
     "parameters": {
-        "ENV_HPT_mode": {"values": [False]},
-        "architecture": {"values": ["LSTM"]},
+        "ENV_HPT_mode": {"values": [True]},
+        "architecture": {"values": ["LSTMTranformer"]},
         "seed": {"values": list(range(1, 6))},
         "online_simulation_factor": {"values": [0, 4]},
         "features": {"values": ["EFs", "GPT4", "BERT"]},
+        "ENV_LEARNING_RATE":{"values":[0.001,0.002,0.0025,0.005]}   
     },
     "command": command
 }
@@ -30,4 +31,4 @@ sweep_config = {
 # Initialize a new sweep
 sweep_id = wandb.sweep(sweep=sweep_config, project=project)
 print("run this line to run your agent in a screen:")
-print(f"screen -dmS \"sweep_agent\" wandb agent {YOUR_WANDB_USERNAME}/{project}/{sweep_id}")
+print(f"screen -dmS \"sweep_agent\" wandb agent {samar2499}/{project}/{sweep_id}")
