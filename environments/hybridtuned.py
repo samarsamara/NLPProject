669a81f8009c8ab903b74cb906574096ec83c2f3
rawanbad <167420452+rawanbad@users.noreply.github.com>
2024-04-23 20:46:30 +0300

    Update hybridtuned.py

--- environments/hybridtuned.py
+++ environments/hybridtuned.py
@@ -1,35 +1,49 @@
 class myhybrid(nn.Module):
-    def __init__(self, n_layers, input_dim, hidden_dim, output_dim, dropout, logsoftmax=True, input_twice=False):
+    def __init__(self,self,config,logsoftmax = True):
         super().__init__()
-        self.n_layers = n_layers
-        self.input_dim = input_dim
-        self.hidden_dim = hidden_dim
-        self.output_dim = output_dim
-        self.dropout = dropout
-        self.input_twice = input_twice
+        nhead = config["transformer_nheads"]
+        input_dim = config["input_dim"]
+        dropout = config["dropout"]
+        nhead = config["transformer_nheads"]
+        hidden_dim = config["hidden_dim"]
+        output_dim = config["output_dim"]
+        n_layers = config["layers"]
 
         self.input_fc = nn.Sequential(nn.Linear(input_dim, input_dim * 2),
                                       nn.Dropout(dropout),
                                       nn.ReLU(),
-                                      nn.Linear(input_dim * 2, self.hidden_dim),
+                                      nn.Linear(input_dim * 2,hidden_dim),
                                       nn.Dropout(dropout),
                                       nn.ReLU())
 
-        self.main_task = nn.LSTM(input_size=self.hidden_dim,
-                                 hidden_size=self.hidden_dim,
+        self.main_task = nn.LSTM(input_size=hidden_dim,
+                                 hidden_size=hidden_dim,
                                  batch_first=True,
-                                 num_layers=self.n_layers,
+                                 num_layers=n_layers,
                                  dropout=dropout)
 
-        seq = [nn.Linear(self.hidden_dim + (input_dim if self.input_twice else 0), self.hidden_dim // 2),
+        seq = [nn.Linear(hidden_dim,hidden_dim // 2),
                nn.ReLU()]
 
         self.output_fc = nn.Sequential(*seq)
         ##building transformer
+        self.fc = nn.Sequential(nn.Linear(input_dim, hidden_dim),
+                                nn.Dropout(dropout),
+                                nn.ReLU(),
+                                ).double()
+
+        self.encoder_layer = nn.TransformerEncoderLayer(d_model=hidden_dim, nhead=nhead, dropout=dropout)
+        self.main_task1 = nn.TransformerEncoder(self.encoder_layer, num_layers=config["layers"]).double()
+
+        self.main_task_classifier = nn.Sequential(nn.Linear(hidden_dim, hidden_dim // 2),
+                                                  nn.ReLU()).double()
+        self.pred = nn.Sequential(nn.Linear(hidden_dim, hidden_dim//2),
+                                                  nn.ReLU(),
+                                                  nn.Linear(hidden_dim//2, 2),
+                                                  nn.LogSoftmax(dim=-1)).double()
         
-      
-        self.user_vectors = UsersVectors(user_dim=self.hidden_dim, n_layers=self.n_layers)
-        self.game_vectors = UsersVectors(user_dim=self.hidden_dim, n_layers=self.n_layers)
+        self.user_vectors = UsersVectors(user_dim=hidden_dim, n_layers=n_layers)
+        self.game_vectors = UsersVectors(user_dim=hidden_dim, n_layers=n_layers)
 
     def init_game(self, batch_size=1):
         return torch.stack([self.game_vectors.init_user] * batch_size, dim=0)
@@ -37,10 +51,16 @@ class myhybrid(nn.Module):
     def init_user(self, batch_size=1):
         return torch.stack([self.user_vectors.init_user] * batch_size, dim=0)
 
-    def forward(self, input_vec, game_vector, user_vector):
+    def forward(self, vectors,**kwargs):
+        input_vec, game_vector, user_vector = (vectors['x'],vectors['game_vector'],vectors['user_vector'])
+        x = self.fc(x)
+        output = []
+        for i in range(DATA_ROUNDS_PER_GAME):
+            time_output = self.main_task1(x[:, :i+1].contiguous())[:, -1, :]
+            output.append(time_output)
+        output = torch.stack(output, 1)
+        output = self.main_task_classifier(output)
         lstm_input = self.input_fc(input_vec)
-        # lstm_input = lstm_input.reshape(-1, self.hidden_dim)
-        # output = self.output_fc(lstm_input)
         lstm_shape = lstm_input.shape
         shape = user_vector.shape
         assert game_vector.shape == shape
@@ -48,17 +68,16 @@ class myhybrid(nn.Module):
             lstm_input = lstm_input.reshape((1,) * (len(shape) - 1) + lstm_input.shape)
         user_vector = user_vector.reshape(shape[:-1][::-1] + (shape[-1],))
         game_vector = game_vector.reshape(shape[:-1][::-1] + (shape[-1],))
-        lstm_output, (game_vector, user_vector) = self.main_task(lstm_input.contiguous(),
+        lstm_output, (game_vec, user_vec) = self.main_task(lstm_input.contiguous(),
                                                                  (game_vector.contiguous(),
                                                                   user_vector.contiguous()))
-        user_vector = user_vector.reshape(shape)
-        game_vector = game_vector.reshape(shape)
-        if hasattr(self, "input_twice") and self.input_twice:
-            lstm_output = torch.cat([lstm_output, input_vec], dim=-1)
-        output = self.output_fc(lstm_output)
-        if len(output.shape) != len(lstm_shape):
-            output.reshape(-1, output.shape[-1])
+        concatenated_output = torch.cat((lstm_output, output), dim=-1)
+        final_output=self.pred(concatenated_output)
+        user_vec = user_vec.reshape(shape)
+        game_vec = game_vec.reshape(shape)
+
         if self.training:
-            return {"output": output, "game_vector": game_vector, "user_vector": user_vector}
+            return {"output": final_output, "game_vector": game_vec, "user_vector": user_vec}
         else:
-            return {"output": output, "game_vector": game_vector.detach(), "user_vector": user_vector.detach()}
+            return {"output": final_output, "game_vector": game_vec.detach(), "user_vector": user_vec.detach()}
+
