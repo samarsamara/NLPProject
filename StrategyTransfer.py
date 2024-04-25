ae88ab78b6bb2f918fe77f538077d0bc428d2fdc
rawanbad <167420452+rawanbad@users.noreply.github.com>
2024-04-25 21:31:22 +0300

    Update StrategyTransfer.py

--- StrategyTransfer.py
+++ StrategyTransfer.py
@@ -8,7 +8,7 @@ from environments.arch2 import arch2_env
 from environments.newenv import arch3_env
 from environments.arch4 import arch4_env
 from environments.newenv2 import arch5_env
-from environments.positional import mytransformerenv
+from environments.positional import mytransformer_env
 
 
 
@@ -96,6 +96,7 @@ meta_features_map = {"features": {"EFs": {"FEATURES_PATH": config["SIMULATION_EF
                                      "arch3": {"use_user_vector": True},
                                      "arch4": {"use_user_vector": True},
                                      "arch5": {"use_user_vector": True},
+                                     "arch6": {"use_user_vector": True},
                                      "hybrid":{"use_user_vector":True}}}
 for meta_feature, meta_feature_map in meta_features_map.items():
     if config[meta_feature] not in meta_feature_map.keys():
@@ -144,5 +145,6 @@ elif config["architecture"] == "arch4":
     env_model = arch4_env(env_name, config=config)
 elif config["architecture"] == "arch5":
     env_model = arch5_env(env_name, config=config)
-
+elif config["architecture"] == "arch6":
+    env_model = mytransformer_env(env_name, config=config)
     
