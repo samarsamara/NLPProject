37bdb0f6aae57e35bc6c9134282192b0907f3f41
samarsamara <166445618+samarsamara@users.noreply.github.com>
2024-04-30 22:00:05 +0300

    Update multiLayer_TranLSTM.py

--- environments/multiLayer_TranLSTM.py
+++ environments/multiLayer_TranLSTM.py
@@ -67,7 +67,7 @@ class multiLayer_TranLSTM(nn.Module):
                 lstm_output, (game_vector, user_vector) = self.lstm(lstm_input.contiguous(),
                                                                          (game_vector.contiguous(),
                                                                         user_vector.contiguous()))
-                transformer_input = lstm_output.reshape((1,) * (len(shape) - 1) + lstm_input.shape)
+                transformer_input = lstm_output.reshape((1,) * (len(shape) - 1) + lstm_output.shape)
             user_vector = user_vector.reshape(shape)
             game_vector = game_vector.reshape(shape)
             output = self.output_fc(lstm_output)
