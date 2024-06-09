5f11f80cb47f6fe012de813f76b4c5126f3628d0
samarsamara <166445618+samarsamara@users.noreply.github.com>
2024-06-09 19:48:12 +0300

    Create sweep4_samarsamara.py

---
+++ RunningScripts/sweep4_samarsamara.py
@@ -0,0 +1,32 @@
+import wandb
+YOUR_WANDB_USERNAME = "rwanbd"
+project = "Strategy_Transfer_TACL"
+
+command = [
+        "${ENVIRONMENT_VARIABLE}",
+        "${interpreter}",
+        "StrategyTransfer.py",
+        "${project}",
+        "${args}"
+    ]
+
+sweep_config = {
+    "name": "TransformerLSTM: 4 heads",
+    "method": "grid",
+    "metric": {
+        "goal": "maximize",
+        "name": "AUC.test.max"
+    },
+    "parameters": {
+        "ENV_HPT_mode": {"values": [True]},
+        "architecture": {"values": ["TransformerLSTM"]},
+        "transformer_nheads":{"values":[4]},
+        "hidden_dim={"values":[32]}
+    },
+    "command": command
+}
+
+# Initialize a new sweep
+sweep_id = wandb.sweep(sweep=sweep_config, project=project)
+print("run this line to run your agent in a screen:")
+print(f"screen -dmS \"sweep_agent\" wandb agent {YOUR_WANDB_USERNAME}/{project}/{sweep_id}")
