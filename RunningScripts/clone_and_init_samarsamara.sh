8521ab03867c1af27ef8898f91ebf9ad50ab5066
samarsamara <166445618+samarsamara@users.noreply.github.com>
2024-06-09 19:43:06 +0300

    Update and rename clone_and_init_YOUR_UID.sh to clone_and_init_samarsamara.sh

---
+++ RunningScripts/clone_and_init_samarsamara.sh
@@ -0,0 +1,21 @@
+# cloning your code from github:
+git clone https://github.com/samarsamara/NLPProject
+cd NLPProject
+cd RunningScripts
+# Your main sweep:
+echo "Running the final sweep script..."
+python final_sweep_samarsamara.py
+cd ..
+# More runs appear in your report:
+cd RunningScripts
+echo "Running the sweep1 script..."
+python sweep_1_samarsamara.py
+cd..
+cd RunningScripts
+echo "Running the sweep2 script..."
+python sweep_2_samarsamara.py
+cd..
+cd RunningScripts
+echo "Running the sweep3 script..."
+python sweep_3_samarsamara.py
+cd..
